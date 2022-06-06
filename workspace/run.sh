c1e9f4fa3826fd17d740291a1434f95e54246534
Juho Röyskö <roysko.juho@gmail.com>
2022-06-06 19:03:29 +0300

    Fix launch file

--- workspace/run.sh
+++ workspace/run.sh
@@ -7,24 +7,22 @@ source install/setup.bash
 if [[ -z $IP && -z $BOT ]]; then
     # Remote
     ros2 launch launch/test_world_2.launch.py >/dev/null 2>&1 &
+    ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True >/dev/null 2>&1 &
 elif [ -z $BOT ]; then
     # Remote -> Raspi
-    ssh ubuntu@$IP 'cd ~/ros2-mapper/workspace && BOT=1 ./run.sh' &
-
-    # TODO: Run the remote part of the software
+    ssh ubuntu@$IP 'cd ~/ros2-mapper/workspace && BOT=1 ./run.sh'
+    exit 0
 else
     # Raspi
     . ~/ros2_ws/install/local_setup.bash
     ros2 launch turtlebot3_bringup robot.launch.py >/dev/null 2>&1 &
     sleep 20
+    ros2 launch turtlebot3_navigation2 navigation2.launch.py >/dev/null 2>&1 &
 fi
 
-if [ -z $IP ]; then
-    ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True >/dev/null 2>&1 &
-    ros2 launch slam_toolbox online_async_launch.py >/dev/null 2>&1 &
-    ros2 launch explore_lite explore.launch.py >/dev/null 2>&1 &
-    ros2 run v4l2_camera v4l2_camera_node >/dev/null 2>&1 &
-    ros2 launch launch/launch.py &
-fi
+ros2 launch slam_toolbox online_async_launch.py >/dev/null 2>&1 &
+ros2 launch explore_lite explore.launch.py >/dev/null 2>&1 &
+ros2 run v4l2_camera v4l2_camera_node >/dev/null 2>&1 &
+ros2 launch launch/launch.py &
 
 wait
