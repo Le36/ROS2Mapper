a6814da6e53adf399333e8d8263348d2696bfe52
Juho Röyskö <roysko.juho@gmail.com>
2022-06-06 17:05:02 +0300

    Fix running project

--- workspace/run.sh
+++ workspace/run.sh
@@ -3,7 +3,7 @@ trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 source install/setup.bash
-ros2 launch turtlebot3_gazebo test_world_1.launch.py >/dev/null 2>&1 &
+ros2 launch launch/test_world_2.launch.py >/dev/null 2>&1 &
 ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True >/dev/null 2>&1 &
 ros2 launch slam_toolbox online_async_launch.py >/dev/null 2>&1 &
 ros2 launch explore_lite explore.launch.py >/dev/null 2>&1 &
