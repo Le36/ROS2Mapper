35b01dfc171c759601d43cd811a87a9724b27687
asianomainen <oskari.nuottonen@gmail.com>
2022-06-06 18:41:04 +0300

    Fix run.sh

--- workspace/run.sh
+++ workspace/run.sh
@@ -7,9 +7,9 @@ source install/setup.bash
 if [[ -z $IP && -z $BOT ]]; then
     # Remote
     ros2 launch launch/test_world_2.launch.py >/dev/null 2>&1 &
-elif [ -z $IP ]; then
+elif [ -z $BOT ]; then
     # Remote -> Raspi
-    ssh ubuntu@$(IP) 'cd ~/ros2-mapper/workspace && BOT=1 ./run.sh' &
+    ssh ubuntu@$IP 'cd ~/ros2-mapper/workspace && BOT=1 ./run.sh' &
 
     # TODO: Run the remote part of the software
 else
@@ -19,7 +19,7 @@ else
     sleep 20
 fi
 
-if [ -z $BOT ]; then
+if [ -z $IP ]; then
     ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True >/dev/null 2>&1 &
     ros2 launch slam_toolbox online_async_launch.py >/dev/null 2>&1 &
     ros2 launch explore_lite explore.launch.py >/dev/null 2>&1 &
