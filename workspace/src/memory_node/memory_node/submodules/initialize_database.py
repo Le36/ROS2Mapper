b37b624c6e629d961ad3dae16316e28038409152
Le36 <48809753+Le36@users.noreply.github.com>
2022-06-09 15:54:43 +0300

    sqlite adjustment

--- workspace/src/memory_node/memory_node/submodules/initialize_database.py
+++ workspace/src/memory_node/memory_node/submodules/initialize_database.py
@@ -6,23 +6,27 @@ def create_tables():
     connection = get_database_connection()
     cursor = connection.cursor()
 
-    cursor.execute("""
+    tables = ["""
         CREATE TABLE IF NOT EXISTS robot
         (
             id   INTEGER PRIMARY KEY,
             data TEXT
         );
+    """, """
         CREATE TABLE IF NOT EXISTS qr
         (
             content     TEXT UNIQUE PRIMARY KEY,
             coordinates TEXT
         );
+    """, """
         CREATE TABLE IF NOT EXISTS history
         (
             content     TEXT,
             coordinates TEXT,
             time        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL PRIMARY KEY
         );
-    """)
+    """]
 
-    connection.commit()
+    for x in tables:
+        cursor.execute(x)
+        connection.commit()
