450c443955de45e952686dfba44a8879491a5f18
Le36 <48809753+Le36@users.noreply.github.com>
2022-06-09 15:10:14 +0300

    db tables for history and qr pos

--- workspace/src/memory_node/memory_node/submodules/initialize_database.py
+++ workspace/src/memory_node/memory_node/submodules/initialize_database.py
@@ -7,9 +7,21 @@ def create_tables():
     cursor = connection.cursor()
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS robot (
-            id INTEGER PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS robot
+        (
+            id   INTEGER PRIMARY KEY,
             data TEXT
+        );
+        CREATE TABLE IF NOT EXISTS qr
+        (
+            content     TEXT UNIQUE PRIMARY KEY,
+            coordinates TEXT
+        );
+        CREATE TABLE IF NOT EXISTS history
+        (
+            content     TEXT UNIQUE PRIMARY KEY,
+            coordinates TEXT,
+            time        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
         );
     """)
 
