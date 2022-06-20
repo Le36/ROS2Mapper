5976d5b2c5174ef865a3a24affa82ed88f7de54b
asianomainen <oskari.nuottonen@gmail.com>
2022-06-20 22:23:21 +0300

    Update launch scripts

--- workspace/run.sh
+++ workspace/run.sh
@@ -6,25 +6,23 @@ source install/setup.bash
 
 if [[ -z $IP && -z $BOT ]]; then
   # Gazebo
-  ros2 launch launch/test_world_2.launch.py use_sim_time:=True >/dev/null 2>&1 &
+  ros2 launch launch/test_world_${1:-2}.launch.py use_sim_time:=True >/dev/null 2>&1 &
   ros2 launch slam_toolbox online_async_launch.py use_sim_time:=True >/dev/null 2>&1 &
   sleep 0.5
   ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True >/dev/null 2>&1 &
   ros2 launch launch/gazebo_launch.py use_sim_time:=True >/dev/null 2>&1 &
   ros2 run io_node control use_sim_time:=True
+  exit 0
 elif [ -z "$BOT" ]; then
   # Remote -> Raspi
-  ssh ubuntu@"$IP" 'cd ~/ros2-mapper/workspace && BOT=1 ./run.sh'
+  ssh ubuntu@"$IP" 'cd ~/ros2-mapper/workspace && BOT=1 ./run.sh' >/dev/null 2>&1 &
+  ros2 launch/remote_launch.py >/dev/null 2>&1 &
+  ros2 run io_node control
   exit 0
 else
   # Raspi
-  . ~/ros2_ws/install/local_setup.bash
   ros2 launch turtlebot3_bringup robot.launch.py >/dev/null 2>&1 &
   sleep 20
-  ros2 launch turtlebot3_navigation2 navigation2.launch.py >/dev/null 2>&1 &
-  ros2 launch slam_toolbox online_async_launch.py >/dev/null 2>&1 &
-  ros2 launch explore_lite explore.launch.py >/dev/null 2>&1 &
-  ros2 launch launch/launch.py &
+  ros2 run camera_node launch >/dev/null 2>&1 
+  wait
 fi
-
-wait
