9302445e52507569a161866cac67beff80e8c5e0
Juho Röyskö <roysko.juho@gmail.com>
2022-06-22 16:38:05 +0300

    Add docstrings to qr_code_reader

--- workspace/src/qr_code_reader/qr_code_reader/math.py
+++ workspace/src/qr_code_reader/qr_code_reader/math.py
@@ -10,6 +10,7 @@ from numpy import ndarray
 
 
 def rotate_x(x_angle: float) -> ndarray:
+    """Get 3d rotation matrix of the x angle"""
     return np.array(
         [
             [1, 0, 0],
@@ -20,6 +21,7 @@ def rotate_x(x_angle: float) -> ndarray:
 
 
 def rotate_y(y_angle: float) -> ndarray:
+    """Get 3d rotation matrix of the y angle"""
     return np.array(
         [
             [cos(y_angle), 0, sin(y_angle)],
@@ -30,6 +32,7 @@ def rotate_y(y_angle: float) -> ndarray:
 
 
 def rotate_z(z_angle: float) -> ndarray:
+    """Get 3d rotation matrix of the z angle"""
     return np.array(
         [
             [cos(z_angle), -sin(z_angle), 0],
@@ -40,11 +43,13 @@ def rotate_z(z_angle: float) -> ndarray:
 
 
 def get_rotation_matrix(rotation: ndarray) -> ndarray:
+    """Get 3d rotation matrix of the euler rotation angles"""
     x_angle, y_angle, z_angle = rotation
     return np.matmul(rotate_z(z_angle), np.matmul(rotate_y(y_angle), rotate_x(x_angle)))
 
 
 def quaternion_to_euler(q: ndarray) -> ndarray:
+    """Convert quaternion angles to euler angles"""
     return np.array(
         [
             [atan2(2 * (q[0] * q[1] + q[2] * q[3]), 1 - 2 * (q[1] ** 2 + q[2] ** 2))],
@@ -55,6 +60,7 @@ def quaternion_to_euler(q: ndarray) -> ndarray:
 
 
 def quaternion_of_vectors(v1: ndarray, v2: ndarray) -> ndarray:
+    """Get the quaternion between two vectors"""
     v1 /= np.linalg.norm(v1)
     v2 /= np.linalg.norm(v2)
     a = np.cross(v1, v2)
