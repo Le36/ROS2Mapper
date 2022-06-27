2492370ee025faec72e938f44d12199f65c8d991
Juho Röyskö <roysko.juho@gmail.com>
2022-06-27 12:50:29 +0300

    Change the publish rate of the camera node

--- workspace/src/camera_node/camera_node/camera_node.py
+++ workspace/src/camera_node/camera_node/camera_node.py
@@ -1,5 +1,5 @@
-import rclpy
 import cv2
+import rclpy
 from cv_bridge import CvBridge
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -16,14 +16,13 @@ class CameraNode(Node):
             self.get_logger().fatal("Could not open camera")
 
         self.publisher = self.create_publisher(Image, "/camera/image_raw", 10)
-        self.image_publisher()
+        self.timer = self.create_timer(1 / 5, self.publish_image)
 
-    def image_publisher(self) -> None:
-        """Publish image from camera"""
-        while True:
-            _, image = self.cap.read()
-            ros_image = self.bridge.cv2_to_imgmsg(image, "bgr8")
-            self.publisher.publish(ros_image)
+    def publish_image(self) -> None:
+        """Publish one image from the camera"""
+        _, image = self.cap.read()
+        ros_image = self.bridge.cv2_to_imgmsg(image, "bgr8")
+        self.publisher.publish(ros_image)
 
 
 def main(args=None) -> None:  # pragma: no cover
