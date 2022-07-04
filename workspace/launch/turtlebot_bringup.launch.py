ebc9554b66ec3eef22ba95940c97a480757e9fc2
Juho Röyskö <roysko.juho@gmail.com>
2022-07-04 11:43:05 +0300

    Update launch script and running instructions to README.md

--- workspace/launch/turtlebot_bringup.launch.py
+++ workspace/launch/turtlebot_bringup.launch.py
@@ -1,11 +1,11 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -22,7 +22,15 @@ def generate_launch_description():
     return LaunchDescription(
         [
             turtlebot3_bringup,
-            Node(package="ros2mapper_camera_node", executable="launch", name="ros2mapper_camera_node"),
-            Node(package="ros2mapper_qr_code_reader", executable="launch", name"ros2mapper_qr_code_reader"),
+            Node(
+                package="ros2mapper_camera_node",
+                executable="launch",
+                name="ros2mapper_camera_node",
+            ),
+            Node(
+                package="ros2mapper_qr_code_reader",
+                executable="launch",
+                name="ros2mapper_qr_code_reader",
+            ),
         ]
     )
